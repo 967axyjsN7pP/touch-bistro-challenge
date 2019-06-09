9d258f4feb4c218b65146bc7e68b87e0be26f271
Steven Smith <steven.76484@gmail.com>
2019-06-09 18:54:46 -0400

    Adding getPrimes function

--- index.js
+++ index.js
@@ -2,6 +2,36 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+// See:
+// https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+// https://stackoverflow.com/a/15471749
+const getPrimes = limit => {
+    const FIRST_PRIME = 2;
+
+    if (!Number.isInteger(limit)) { throw new Error('Invalid Argument: `limit` must be an Integer'); }
+	if (limit <= FIRST_PRIME) { throw new Error('Invalid Argument: `limit` must be greater than 2.'); }
+
+    const result = new Array(limit).fill(true);
+    const squareRootOfLimit = Math.floor(Math.sqrt(limit));
+
+    result[0] = false;
+    result[1] = false;
+
+    for (let i = FIRST_PRIME; i <= squareRootOfLimit; i += 1) {
+        if (!result[i]) { continue; }
+        for (let j = i * i; j < limit; j += i) {
+            result[j] = false;
+        }
+    }
+
+    return result.reduce((acc, el, index) => {
+        if (el) {
+            acc.push(index)
+        }
+        return acc;
+    }, []);
+};
+
 app.get('/', (req, res) => {
     const limit = Number(req.query.limit)
     // TODO: Validate `limit`. (bounds?)
@@ -12,7 +42,8 @@ app.get('/', (req, res) => {
     // TODO: Compute actual result from limit query.
     const result = {
         limit,
-        medianPrimes: [3, 5]
+        medianPrimes: [3, 5],
+        primes: getPrimes(limit)
     };
 
     res.status(200).json(result);
